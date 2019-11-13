cb522bb0b8697228bb067618bfe6cc4daba19cfc
jeongsunjae <azuretic94@gmail.com>
2019-11-13 23:06:04 +0900

    Updates

---
+++ precache-manifest.fc6b7b3e167eef2df142188080e2c442.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6193d430d11028b3ba49bdf3717980",
+    "url": "/movieql-client/index.html"
+  },
+  {
+    "revision": "fdf295287bfbded119e9",
+    "url": "/movieql-client/static/js/2.a7a4486e.chunk.js"
+  },
+  {
+    "revision": "86a22331f8454ebb8195",
+    "url": "/movieql-client/static/js/main.96839de8.chunk.js"
+  },
+  {
+    "revision": "9855e948e92015a37626",
+    "url": "/movieql-client/static/js/runtime-main.118d691c.js"
+  }
+]);
